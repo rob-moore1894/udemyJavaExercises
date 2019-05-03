547a36619edcbce435d221859d4f36fd556f4037
rob-moore1894 <moore.re@gmail.com>
2019-05-03 14:30:57 -0500

    Completed Equality Printer and Playing Cat Exercises

--- src/EqualityPrinter.java
+++ src/EqualityPrinter.java
@@ -1,2 +1,25 @@
 public class EqualityPrinter {
+
+    public static void printEqual(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0){
+            System.out.println("Invalid Value");
+        } else if (a == b && b == c){
+            System.out.println("All numbers are equal");
+        } else if (a != b && b != c){
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    } // End of printEqual()
+
+    public static void printEqual(){
+        System.out.println("Neither all are equal or different");
+    }
+
+    public static void main(String[] args) {
+        printEqual(1, 1, 1);
+        printEqual(1, 0, 1);
+        printEqual(-1, -2, -4);
+        printEqual();
+    }
 }
